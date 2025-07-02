c0215601616813bcc84de12e8a61e049e288c0c8
Carlos Daniel Teodoro da SIlva <143669778+carlossilva2001@users.noreply.github.com>
2025-07-02 20:00:36 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -6,21 +6,9 @@ function IniciarJogoMedia(){
 let quantidade = prompt("Quantos números você quer digitar?");
 quantidade = Number(quantidade);
 
-if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
-  alert("Quantidade inválida. Tente novamente com um número maior que zero.");
-} else {
-  let soma = 0;
-
   for (let i = 1; i <= quantidade; i++) {
     let entrada = prompt(`Digite o ${i}º número:`);
     let numero = Number(entrada);
-
-    if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
-      alert("Valor inválido. Digite um número.");
-      i--;
-      continue;
-    }
-
     soma += numero;
   }
 
